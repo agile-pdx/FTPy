743831af98c1296148f623cbe0f0bce477d5620e
rpenziol <rpenziol@pdx.edu>
2015-07-13 23:55:44 -0700

    Added initial argument parsing

--- ftp.py
+++ ftp.py
@@ -1,4 +1,13 @@
 import argparse
 import pysftp
 
-print "Hello World"
+#Grab user command-line input
+parser = argparse.ArgumentParser()
+parser.add_argument('-url', '--url')
+parser.add_argument('-u', '--username')
+parser.add_argument('-p', '--password')
+args = parser.parse_args()
+
+print "Attempting to connect to " + args.url + " as '" + args.username + "'..."
+
+#TO DO: Add code to connect to server
