e87e4b8254b87a7de3dd042a291fcd687406f19b
bushj <Joel.H.Bush@Gmail.com>
2015-07-16 17:37:26 -0700

    Reversed -h and --help changes

--- ftp.py
+++ ftp.py
@@ -5,7 +5,6 @@ import pysftp
 parser = argparse.ArgumentParser()
 parser.add_argument('-url', '--url')
 parser.add_argument('-u', '--username')
-parser.add_argument('-h', '--help')
 args = parser.parse_args()
 
 #Avoid getting password from command-line argument for enhanced security
